101e0f9d2706266621dea446ea977b121f3094a2
slezkinis <slezkin.is@mail.ru>
2022-08-29 15:51:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ import random
 from dotenv import load_dotenv
 
 
-def download_file(url, params, path):
-    response = requests.get(url, params=params)
+def download_file(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
     with open(path, 'wb') as file:
         file.write(response.content)
 
 
-def upload_file(file_name):
-    with open(file_name, 'rb') as file:
-        files = {
-            'photo': file
-        }
-        response = requests.post(upload_url, files=files)
+def upload_file(file_name, upload_url):
+    try:
+        with open(file_name, 'rb') as file:
+            files = {
+                'photo': file
+            }
+            response = requests.post(upload_url, files=files)
         response.raise_for_status()
-    os.remove(file_name)
-    return response.json()
+        return response.json()
+    finally:
+        os.remove(file_name)
 
 
 def get_last_comic_issue():
@@ -42,7 +45,7 @@ def get_upload_url(vk_token, group_id, version):
     return response.json()['response']['upload_url']
 
 
-def public_photo(group_id, text, owner_id, media_id, vk_token, version):
+def post_comic(group_id, text, owner_id, media_id, vk_token, version):
     payload = {
         'owner_id': f'-{group_id}',
         'message': text,
@@ -56,14 +59,14 @@ def public_photo(group_id, text, owner_id, media_id, vk_token, version):
     response.raise_for_status()
 
 
-def save_wall_photo(vk_token, group_id, version, photo, server, hash):
+def save_comic(vk_token, group_id, version, photo, server, server_hash):
     payload = {
         'access_token': vk_token,
         'group_id': group_id,
         'v': version,
         'photo': photo,
         'server': server,
-        'hash': hash
+        'hash': server_hash
     }
     response = requests.post(
         'https://api.vk.com/method/photos.saveWallPhoto',
@@ -73,15 +76,14 @@ def save_wall_photo(vk_token, group_id, version, photo, server, hash):
     return response.json()['response'][0]
 
 
-def get_comic_url(comic_number):
+def get_comic(comic_number):
     url = f'https://xkcd.com/{comic_number}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
-    about_comic = response.json()
-    return about_comic
+    return response.json()
 
 
-if __name__ == '__main__':
+def main():
     load_dotenv()
     vk_token = os.environ['VK_TOKEN']
     version = 5.131
@@ -89,24 +91,28 @@ if __name__ == '__main__':
     file_name = 'file.png'
     comics_amount = get_last_comic_issue()
     comic_number = random.randint(0, comics_amount)
-    about_comic = get_comic_url(comic_number)
-    image_url = about_comic['img']
-    download_file(image_url, {}, file_name)
+    received_comic = get_comic(comic_number)
+    image_url = received_comic['img']
+    download_file(image_url, file_name)
     upload_url = get_upload_url(vk_token, group_id, version)
-    about_upload_photo = upload_file(file_name)
-    about_image = save_wall_photo(
+    upload_comic = upload_file(file_name, upload_url)
+    about_save_comic = save_comic(
         vk_token,
         group_id,
         version,
-        about_upload_photo['photo'],
-        about_upload_photo['server'],
-        about_upload_photo['hash']
+        upload_comic['photo'],
+        upload_comic['server'],
+        upload_comic['hash']
     )
-    public_photo(
+    post_comic(
         group_id,
-        about_comic['alt'],
-        about_image['owner_id'],
-        about_image['id'],
+        received_comic['alt'],
+        about_save_comic['owner_id'],
+        about_save_comic['id'],
         vk_token,
         version
     )
+
+
+if __name__ == '__main__':
+    main()
